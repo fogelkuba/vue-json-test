9c47c98ffbed0152542983b82dcc6700430355cc
fogelkuba <fogelkuba@gmail.com>
2019-07-25 22:08:12 +0200

    display test data

--- src/components/ContectComponent.vue
+++ src/components/ContectComponent.vue
@@ -1,10 +1,17 @@
 <template>
-
+    <div>
+        <h2>Contact</h2>
+        <pre>
+            {{data}}
+        </pre>
+    </div>
 </template>
 
 <script>
+/* eslint-disable */
     export default {
-        name: "ContectComponent"
+        name: "ContectComponent",
+        props: ['data']
     }
 </script>
 
--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,10 +1,17 @@
 <template>
-
+    <div>
+        <h1>Main</h1>
+        <pre>
+            {{data}}
+        </pre>
+    </div>
 </template>
 
 <script>
+/* eslint-disable */
     export default {
-        name: "MainComponent"
+        name: "MainComponent",
+        props: ['data']
     }
 </script>
 
