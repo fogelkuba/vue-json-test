cd48c4facc83ad5bf0677e834e1c850f4b3265fa
fogelkuba <fogelkuba@gmail.com>
2019-07-25 22:07:14 +0200

    data tefector to .js

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
-{
+export default {
   "main": {
     "title": "Hello to my site!",
     "description": "Check out this awesome Project"
--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,36 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <MainComponent/>
-    <ContectComponent/>
+    <MainComponent />
+    <ContectComponent />
+<!--    {{ jsonData }}-->
   </div>
 </template>
 
 <script>
-import MainComponent from "~/components/MainComponent";
-import ContectComponent from "~/components/ContectComponent";
+/* eslint-disable */
+
+import MainComponent from "./components/MainComponent";
+import ContectComponent from "./components/ContectComponent";
+
+import json from "../data";
 
 export default {
   name: "app",
+  data() {
+    return {
+      // jsonData: JSON.parse(json)
+    };
+  },
   components: {
     MainComponent,
     ContectComponent
+  },
+  computed: {
+    jsonData() {
+      console.log(json);
+      return json;
+    }
   }
 };
 </script>
