27d9f6b1b92f3bf2c3c6981d90bd7916c7d4388d
fogelkuba <fogelkuba@gmail.com>
2019-07-31 22:22:53 +0200

    fix

--- data.js
+++ data.js
@@ -1,10 +1,11 @@
 export default {
   main: {
-    "title": "Hello to my site!",
-    "description": "Check out this awesome Project"
+    title: "Hello to my site!",
+    description: "Check out this awesome Project",
+    subtitle: "Check it out!"
   },
   contact: {
-    "email": "test@test.com",
-    "phone": "+48 111 222 333"
+    email: "test@test.com",
+    phone: "+48 111 222 333"
   }
-}
+};
